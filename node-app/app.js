f5b541d7f7f49b60a0a52677915034cd3a567902
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2022-09-07 22:35:46 +0530

    nodemon install

--- node-app/app.js
+++ node-app/app.js
@@ -1,14 +1,24 @@
+// command
+//npm init to create pakage.json file
+//nodemon fileName.js
+
 // const add = require('../node-app/utils');
 // const messageShow = require('../node-app/utils');
 // const sum = add(19, 5);
 // console.log(sum)
 // console.log(messageShow)
 
-//npm init to create pakage.json file
-const validator = require('validator')
+// const validator = require('validator')
 
-console.log(validator.isEmail('mindgo.sharma@gmail.com'))
-console.log(validator.isEmail('mindgo.sharma@gmail.com11'))
+// console.log(validator.isEmail('mindgo.sharma@gmail.com'))
+// console.log(validator.isEmail('mindgo.sharma@gmail.com11'))
 
-console.log(validator.isURL('https://google.com'))
-console.log(validator.isURL('https/google.com'))
+// console.log(validator.isURL('https://google.com'))
+// console.log(validator.isURL('https/google.com'))
+
+// const chalk = require('chalk');
+// console.log(chalk.blue('Hello world!'));
+
+const chalk = require('chalk')
+const successMsge = chalk.green('Success!!')
+console.log(successMsge)
