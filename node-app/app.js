e32132de1d9f41a5eb62f2afa9e0724ca41dc4cd
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2022-09-07 21:29:36 +0530

    package.js created and validator npm is used

--- node-app/app.js
+++ node-app/app.js
@@ -1,5 +1,14 @@
-const add = require('../node-app/utils');
-const messageShow = require('../node-app/utils');
-const sum = add(19, 5);
-console.log(sum)
-console.log(messageShow)
+// const add = require('../node-app/utils');
+// const messageShow = require('../node-app/utils');
+// const sum = add(19, 5);
+// console.log(sum)
+// console.log(messageShow)
+
+//npm init to create pakage.json file
+const validator = require('validator')
+
+console.log(validator.isEmail('mindgo.sharma@gmail.com'))
+console.log(validator.isEmail('mindgo.sharma@gmail.com11'))
+
+console.log(validator.isURL('https://google.com'))
+console.log(validator.isURL('https/google.com'))
