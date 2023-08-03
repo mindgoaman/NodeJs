48c2358b76195f70d0eec3cc9663e6c7a5a09375
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-03 23:33:26 +0530

    single object send by create data file

---
+++ NODE_JS/data.js
@@ -0,0 +1,2 @@
+const data = {name: 'Aman Sharma', email: 'mindgo.aman@gmail.com', mobile: 8929717151}
+module.exports = data
--- NODE_JS/index.js
+++ NODE_JS/index.js
@@ -1,6 +1,7 @@
+const data = require('./data')
 const http = require('http');
 http.createServer((req, resp)=>{
      resp.writeHead(200,{'Content-Type':'application/json'})
-     resp.write(JSON.stringify({name: 'Aman Sharma', email: 'mindgo.aman@gmail.com', mobile: 8929717151}))
+     resp.write(JSON.stringify(data))
      resp.end()
 }).listen(3000);
