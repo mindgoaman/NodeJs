1a4d1b12997da332a28944f535a2061d1f5e2685
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-03 23:35:49 +0530

    multiple objects send by create data file

--- NODE_JS/data.js
+++ NODE_JS/data.js
@@ -1,2 +1,8 @@
-const data = {name: 'Aman Sharma', email: 'mindgo.aman@gmail.com', mobile: 8929717151}
+const data = [
+    {name: 'Aman Sharma', email: 'mindgo.aman@gmail.com', mobile: 8929717151},
+    {name: 'Rahul Ranjan', email: 'mindgo.ranjan@gmail.com', mobile: 8929717152},
+    {name: 'Rahul Kumar', email: 'mindgo.rahul@gmail.com', mobile: 8929717153},
+    {name: 'Manish Kumar', email: 'mindgo.manish@gmail.com', mobile: 8929717154},
+    {name: 'Sahul Kumar', email: 'mindgo.sahul@gmail.com', mobile: 8929717154},
+]
 module.exports = data
