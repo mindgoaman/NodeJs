4a9cb18658b4b9bd7050ffaa88057bc601e4d557
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-05 22:21:47 +0530

    file create and delete via command line input

--- NODE_JS/index.js
+++ NODE_JS/index.js
@@ -1,7 +1,25 @@
-const data = require('./data')
-const http = require('http');
-http.createServer((req, resp)=>{
-     resp.writeHead(200,{'Content-Type':'application/json'})
-     resp.write(JSON.stringify(data))
-     resp.end()
-}).listen(3000);
+// server creation and data send
+// const data = require('./data')
+// const http = require('http');
+// http.createServer((req, resp)=>{
+//      resp.writeHead(200,{'Content-Type':'application/json'})
+//      resp.write(JSON.stringify(data))
+//      resp.end()
+// }).listen(3000);
+
+//command line input
+const fs = require('fs')
+const path = require('path')
+const dirPath = path.join(__dirname, 'files')
+const inputRec = process.argv
+console.log(inputRec)
+
+if (inputRec[2] == 'add') {
+     console.log(`${dirPath}${'/'}${inputRec[3]}`)
+     fs.writeFileSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
+} else if (inputRec[2] == 'remove') {
+     console.log(`${dirPath}${'/'}${inputRec[3]}`)
+     fs.unlinkSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
+}else{
+     console.log('Not valid input')
+}
