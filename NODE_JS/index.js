c09b854afbfab1e0dba7641a834c490cf0447dd7
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-05 22:26:24 +0530

    file create and delete via command line

--- NODE_JS/index.js
+++ NODE_JS/index.js
@@ -8,18 +8,45 @@
 // }).listen(3000);
 
 //command line input
-const fs = require('fs')
-const path = require('path')
-const dirPath = path.join(__dirname, 'files')
-const inputRec = process.argv
-console.log(inputRec)
+// const fs = require('fs')
+// const path = require('path')
+// const dirPath = path.join(__dirname, 'files')
+// const inputRec = process.argv
+// console.log(inputRec)
 
-if (inputRec[2] == 'add') {
-     console.log(`${dirPath}${'/'}${inputRec[3]}`)
-     fs.writeFileSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
-} else if (inputRec[2] == 'remove') {
-     console.log(`${dirPath}${'/'}${inputRec[3]}`)
-     fs.unlinkSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
-}else{
-     console.log('Not valid input')
-}
+// if (inputRec[2] == 'add') {
+//      console.log(`${dirPath}${'/'}${inputRec[3]}`)
+//      fs.writeFileSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
+// } else if (inputRec[2] == 'remove') {
+//      console.log(`${dirPath}${'/'}${inputRec[3]}`)
+//      fs.unlinkSync(`${dirPath}${'/'}${inputRec[3]}`, inputRec[4])
+// }else{
+//      console.log('Not valid input')
+// }
+
+//multiple files creation and read multiples files
+// const { error } = require('console');
+// const fs = require('fs');
+// const dir = require('path');
+// const dirPath = dir.join(__dirname, 'files');
+
+// //createFile
+// const createFile = () => {
+//      for(let i=0; i<5; i++){
+//           console.log(i)
+//           fs.writeFileSync(`${dirPath}${'/sharma'}${i}${'.txt'}`, `${'This is'} ${i} ${'is created inside files folder.'}`)
+//      }
+// }
+
+// //readFiles
+// const readFiles = () => {
+//      fs.readdir(dirPath,(err, filesread)=>{
+//           filesread.map((item)=>{
+//                console.log(`${'file name is'} ${item}`)
+//           })
+//      })
+   
+// }
+
+//createFile()
+// readFiles()
