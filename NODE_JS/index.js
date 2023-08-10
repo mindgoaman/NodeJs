122a6d5b4f80abceab179c210e57b9bf137e4128
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-10 23:25:00 +0530

    database connection

--- NODE_JS/index.js
+++ NODE_JS/index.js
@@ -50,3 +50,80 @@
 
 //createFile()
 // readFiles()
+
+
+//crud operation on file system
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'crud/')
+
+//create file
+// const createFile = () => {
+//     fs.writeFileSync(`${dirPath}${'sharma.txt'}`, "This is crud operation file create.")
+// }
+
+// createFile()
+
+// const readFile = () => {
+//     fs.readFile(`${dirPath}${'sharma.txt'}`, 'utf-8', (error, item)=> {
+//         console.log('readfilecontent',item)
+//     })
+// }
+
+// readFile()
+
+// const updateFileContent = () => {
+//      fs.appendFile(`${dirPath}${'sharma.txt'}`, 'Content of file has been changed', (error)=> {
+//         if(!error)
+//          console.log("file is updated.")
+//      })
+// }
+
+// updateFileContent()
+
+//rename file
+// const renameFile = ( ) => {
+//     fs.rename(`${dirPath}${'sharma.txt'}`, `${dirPath}${'rename.txt'}`, (err)=>{
+//          if(!err){
+//             console.log("file name has been changed.")
+//          }
+//     })
+// }
+
+// renameFile()
+
+
+//delete file
+// const deleteFile = () => {
+//     fs.unlink(`${dirPath}${'rename.txt'}`, (err)=>{
+//         if(!err){
+//             console.log("file has been deleted")
+//         }else{
+//             console.log("error",err)
+//         }
+//     })
+    
+// }
+
+// deleteFile()
+
+
+
+//mondgo db connection with node js
+
+const {MongoClient} = require('mongodb');
+const mongodbPathURL = 'mongodb://localhost:27017';
+const client = new MongoClient(mongodbPathURL);
+const database = 'e-comm'
+
+const getData = async () => {
+    let result = await client.connect()
+    let db = result.db(database)
+    let collection = db.collection('products')
+    let response = await collection.find({}).toArray()
+    console.log(response)
+
+}
+
+getData()
+
