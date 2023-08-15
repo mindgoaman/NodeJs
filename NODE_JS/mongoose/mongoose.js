0370a1a4e87f61dcf59f153334a8e4e1655eb8b8
vibhishan_sharma17 <vibhishan.sharma@girnarsoft.co.in>
2023-08-15 11:27:35 +0530

    searching on single and multiple values

--- NODE_JS/mongoose/mongoose.js
+++ NODE_JS/mongoose/mongoose.js
@@ -43,15 +43,61 @@
 // updateDataInDB()
 // deleteDataInDB()
 
+// const { ProductsModel } = require('../models/ProductsModel');
+// require('./config');
+
+// const readDataFromDB = async () => {
+//     // for saving data
+//     // const products = ProductsModel({name: 'Iphone 29 pro', brand: 'Apple', price: 225000, category: 'Electronics'});
+//     const result =  await ProductsModel.find();
+//     console.log("sharma6655",result);
+
+// }
+
+// readDataFromDB()
+
+
+
 const { ProductsModel } = require('../models/ProductsModel');
+const express = require('express');
+const app = express();
 require('./config');
+app.use(express.json())
 
-const readDataFromDB = async () => {
-    // const products = ProductsModel({name: 'Iphone 29 pro', brand: 'Apple', price: 225000, category: 'Electronics'});
-    const result =  await ProductsModel.find();
-    console.log("sharma6655",result);
+//create or save data in db
+app.post('/create-products', async(req, resp) => {
+    const products = ProductsModel(req.body)
+    const result = await products.save()
+    console.log(req.body)
+    resp.send(result)
+})
 
-}
+app.get('/get-products', async(req, resp) => {
+    const result = await ProductsModel.find()
+    resp.send(result)
+})
 
-readDataFromDB()
+app.put('/update-product/:_id',async(req, resp)=>{
+    const result = await ProductsModel.updateOne(req.params,{$set:req.body})
+    console.log(result)
+    resp.send(result)
+})
 
+app.delete('/delete-product/:_id', async(req, resp)=>{
+    const result = await ProductsModel.deleteOne(req.params)
+    console.log(result)
+    resp.send(result)
+})
+
+//search on single field and multiples fields
+app.get('/search/:key', async(req, resp)=>{
+     const result = await ProductsModel.find({
+        "$or": [
+             {name: {$regex:req.params.key}},
+             {category: {$regex:req.params.key}}
+        ]
+     })
+     resp.send(result)
+})
+
+app.listen(3000)
